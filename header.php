7880b1db50164a6c7535d524b0a96855b152e328
Mrozidlo <dev.mroz@gmail.com>
2014-05-05 10:53:28 +0200

    RWD

--- header.php
+++ header.php
@@ -2,14 +2,15 @@
 <html>
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo bloginfo('name'); ?></title>
     
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-    <link rel="stylesheet" type="text/css" media="all" href="<?php echo IMAGELIST_THEME_URL ?>css/screen.min.css" />
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    	<link rel="stylesheet" type="text/css" media="all" href="<?php echo IMAGELIST_THEME_URL ?>css/screen.min.css" />
+    	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
     
-    <?php wp_head(); ?>
+    	<?php wp_head(); ?>
 </head>
 <body>
 
